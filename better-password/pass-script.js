ba454972e6911497594eb926c9e9ce303a76142f
katy01101011 <katy.91560@gmail.com>
2022-04-06 16:28:23 +0200

    fix: pseudocodice

--- better-password/pass-script.js
+++ better-password/pass-script.js
@@ -6,17 +6,31 @@ console.log(number, typeof(number));
 const userName = prompt('Qual\'è il tuo nome?')
 console.log(userName, typeof(userName));
 
+// 2.a Se il nome è lungo almeno 6 lettere, prendi i primi 3 caratteri pari
+// 2.a.a Rendi il secondo carattere maiuscolo
+
+// 2.b Altrimenti prendi tutto il nome
+
 // 3. Chiedere cognome utente
 const userSurname = prompt('Qual\'è il tuo cognome?')
 console.log(userSurname, typeof(userSurname));
+
+// 3.a Se il cognome è lungo almeno 5 lettere, prendi i primi 3 caratteri dispari
+// 3.a.a Rendi il secondo carattere maiuscolo
+
+// 3.b Altrimenti prendi tutto il cognome
 
 // 4. Chiedere il colore preferito
 const favouriteColor = prompt('Qual\'è il tuo colore preferito?')
 console.log(favouriteColor, typeof(favouriteColor));
 
-// 5. Generare password: 'nomecognomecolore21'
-const password = `La tua password è: ${userName}${userSurname}${favouriteColor}${number}`
-console.log(password)
+// 4.a Rendere il secondo carattere maiuscolo
+
+// 5. Generare password: ' '
+// 5.a longName + longSurname + COLOR + 21
+// 5.b shortName + shortSurname + COLOR + 21
+// 5.c longName + shortSurname + COLOR + 21
+// 5.d shortName + longSurname + COLOR + 21
 
 // 6. Visualizzare password sulla pagina WEB
 document.getElementById('your-password').innerHTML = password
