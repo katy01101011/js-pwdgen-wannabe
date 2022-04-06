c6c03a47018de9bcff5a4f38b8ed545b8c233bf1
katy01101011 <katy.91560@gmail.com>
2022-04-06 15:54:41 +0200

    add: password generator

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
-// Test
-const ciao = prompt('Salutami')
-console.log(ciao)
+// 1. costante: 21
+const number = 21
+console.log(number, typeof(number));
+
+// 2. Chiedere nome utente
+const userName = prompt('Qual\'è il tuo nome?')
+console.log(userName, typeof(userName));
+
+// 3. Chiedere cognome utente
+const userSurname = prompt('Qual\'è il tuo cognome?')
+console.log(userSurname, typeof(userSurname));
+
+// 4. Chiedere il colore preferito
+const favouriteColor = prompt('Qual\'è il tuo colore preferito?')
+console.log(favouriteColor, typeof(favouriteColor));
+
+// 5. Generare password: 'nomecognomecolore21'
+const password = `La tua password è: ${userName}${userSurname}${favouriteColor}${number}`
+console.log(password)
+
+// 6. Visualizzare password sulla pagina WEB
+document.getElementById('your-password').innerHTML = password
+
+
