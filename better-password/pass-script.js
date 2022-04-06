dce633e37482bf2d27ad5b13e30ebda1f28ab7eb
katy01101011 <katy.91560@gmail.com>
2022-04-06 16:22:36 +0200

    pseudocodice 'Better Password'

---
+++ better-password/pass-script.js
@@ -0,0 +1,24 @@
+// 1. costante: 21
+const number = 21
+console.log(number, typeof(number));
+
+// 2. Chiedere nome utente
+const userName = prompt('Qual\'è il tuo nome?')
+console.log(userName, typeof(userName));
+
+// 3. Chiedere cognome utente
+const userSurname = prompt('Qual\'è il tuo cognome?')
+console.log(userSurname, typeof(userSurname));
+
+// 4. Chiedere il colore preferito
+const favouriteColor = prompt('Qual\'è il tuo colore preferito?')
+console.log(favouriteColor, typeof(favouriteColor));
+
+// 5. Generare password: 'nomecognomecolore21'
+const password = `La tua password è: ${userName}${userSurname}${favouriteColor}${number}`
+console.log(password)
+
+// 6. Visualizzare password sulla pagina WEB
+document.getElementById('your-password').innerHTML = password
+
+
